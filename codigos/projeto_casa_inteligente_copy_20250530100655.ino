f977c6dc062168a2d4794145bdf4f56a91bf1a46
Cauhê <cauhesanchesmatheus@gmail.com>
2025-06-06 16:48:12 -0300

    Update projeto_casa_inteligente_copy_20250530100655.ino

--- codigos/projeto_casa_inteligente_copy_20250530100655.ino
+++ codigos/projeto_casa_inteligente_copy_20250530100655.ino
@@ -7,43 +7,41 @@
 #include <Servo.h>
 
 //acenderledaodetectarpresenca
-const int PIR = 2; 
+const int PIR = 2;
 const int led = 13;
 
 //verificarvazamentodegas
 const int MQ135 = A0;
-const int bagulhotagarela = 12;   
+const int bagulhotagarela = 12;
 
-Servo motor; //variavel servo
+Servo motor;  //variavel servo
 
 //motor rele
 const int rele = 7;
 
-void acenderledaodetectarpresenca(){ 
-int estadoPIR = digitalRead(PIR);
- 
-if(estadoPIR == HIGH){
-digitalWrite(led, HIGH);
-Serial.println("LED ligado");
+void acenderledaodetectarpresenca() {
+  int estadoPIR = digitalRead(PIR);
+
+  if (estadoPIR == HIGH) {
+    digitalWrite(led, HIGH);
+    Serial.println("LED ligado");
+  } else {
+    digitalWrite(led, LOW);
+    Serial.println("LED desligado");
+  }
 }
-else{
-digitalWrite(led, LOW);
-Serial.println("LED desligado");
-}
-}
-void verificarvazamentodegas(){
-int estadoMQ135 = analogRead(MQ135);
-if(estadoMQ135 >= 200){
-  alarme_dois_tons();
-}
-else{
-  noTone(bagulhotagarela);
-}
+void verificarvazamentodegas() {
+  int estadoMQ135 = analogRead(MQ135);
+  if (estadoMQ135 >= 200) {
+    alarme_dois_tons();
+  } else {
+    noTone(bagulhotagarela);
+  }
 }
 
 void alarme_dois_tons() {
   int freqAlta = 2000;
-  int freqBaixa = 800; 
+  int freqBaixa = 800;
   int duracaoTom = 250;
 
   tone(bagulhotagarela, freqAlta, duracaoTom);
@@ -53,80 +51,93 @@ void alarme_dois_tons() {
 }
 
 #define DHTPINO A1
-#define DHTTYPE DHT11 
+#define DHTTYPE DHT11
 
 DHT dht(DHTPINO, DHT11);
 LiquidCrystal_I2C LCD(0x27, 20, 4);
 
-void verificartemperaturaeumidade(){
-float temperatura = dht.readTemperature();
-float umidade = dht.readHumidity();
-//Serial.println("Temperatura: " + String(temperatura) + "C");
-//Serial.println("Umidade: " + String(umidade) + "%"); 
+void verificartemperaturaeumidade() {
+  float temperatura = dht.readTemperature();
+  float umidade = dht.readHumidity();
+  //Serial.println("Temperatura: " + String(temperatura) + "C");
+  //Serial.println("Umidade: " + String(umidade) + "%");
   LCD.clear();
   LCD.setCursor(0, 0);
-   LCD.print("Temperatura: " + String(temperatura) + "C"); 
+  LCD.print("Temperatura: " + String(temperatura) + "C");
 
-   LCD.setCursor(0, 1);
-   LCD.print("Umidade: " + String(umidade) + "%"); 
+  LCD.setCursor(0, 1);
+  LCD.print("Umidade: " + String(umidade) + "%");
   delay(700);
 }
 
-void abrirporta(){
-motor.write(125);
-delay(3000);
+void abrirporta() {
+  motor.write(125);
 }
 
-void fecharporta(){
-motor.write(0);
-delay(3000);
+void fecharporta() {
+  motor.write(0);
 }
 
-void reletrancar(){
-digitalWrite(rele, LOW);
-Serial.println("trancado");
-delay(3000);
+void reletrancar() {
+  digitalWrite(rele, LOW);
+  Serial.println("trancado");
+  delay(5000);
 }
 
-void reledestrancar(){
-digitalWrite(rele, HIGH);
-Serial.println("destrancado");
-delay(3000);
+void reledestrancar() {
+
+ String senhaD;
+
+ Serial.println("digita a senha");
+while( ! Serial.available());
+senhaD = Serial.readString();
+
+if(senhaD == "pao de batata"){
+  digitalWrite(rele, HIGH);
+  Serial.println("destrancado");
+  abrirporta();
+  delay(5000);
+}
+else{
+Serial.println("Senha incorreta"); 
+fecharporta();
+}  
 }
 
-void setup()
-{
-   pinMode(led, OUTPUT);
-   pinMode(PIR, OUTPUT); 
-   pinMode(bagulhotagarela, OUTPUT);
-   pinMode(MQ135, INPUT); 
-   motor.attach(3); 
-   motor.write(0); // angulo inicial 
-   Serial.begin(9600);
-   dht.begin(); //iniciar o dht 
-   LCD.init(); //inicia o LDC 
-   LCD.backlight(); //liga o led traseiro
-   pinMode(rele, OUTPUT);
+void setup() {
+  Serial.begin(9600);
 
-   digitalWrite(rele, LOW);
+  pinMode(led, OUTPUT);
+  pinMode(PIR, OUTPUT);
+  pinMode(bagulhotagarela, OUTPUT);
+  pinMode(MQ135, INPUT);
+  pinMode(rele, OUTPUT);
+  motor.attach(3);
+  motor.write(0);  // angulo inicial
 
-   Serial.println("Sensores sendo calibrados, aguente >:(");
-   LCD.setCursor(0, 0);
-   LCD.print("Iniciando"); 
-   delay(5000);
-   LCD.setCursor(0, 1);
-   LCD.print("Obrigado"); 
-   Serial.println("Sensores calibrados!");
-   delay(1000);
+  dht.begin();      //iniciar o dht
+  LCD.init();       //inicia o LDC
+  LCD.backlight();  //liga o led traseiro
+
+
+  digitalWrite(rele, LOW);
+
+  Serial.println("Sensores sendo calibrados, aguente >:(");
+  LCD.setCursor(0, 0);
+  LCD.print("Iniciando");
+  delay(5000);
+  LCD.setCursor(0, 1);
+  LCD.print("Obrigado");
+  Serial.println("Sensores calibrados!");
+  delay(1000);
 }
 
-void loop()
-{
+void loop() {
   //acenderledaodetectarpresenca();
   //verificarvazamentodegas();
   //verificartemperaturaeumidade();
- // abrirporta();
- // fecharporta();
+  // abrirporta();
+  // fecharporta();
   reletrancar();
   reledestrancar();
 }
